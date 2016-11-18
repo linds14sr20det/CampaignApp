9d89021260c44f64d90d7acd6e3a730258398c01
Stephen Lind <stephen.l@getjobber.com>
2016-11-18 00:01:40 -0700

    game feed and tests are in, submission form partially in

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,26 @@
 class GamesController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :destroy]
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = current_user.games.build(game_params)
+    if @game.save
+      flash[:success] = "Game created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:location, :win)
+  end
 end
